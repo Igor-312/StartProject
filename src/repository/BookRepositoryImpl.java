4cc60aa13e5d11ab128ae3f18401f83f402da24b
BogdanF <sovtour@gmail.com>
2024-10-30 18:55:02 +0100

    change BookRepository

--- src/repository/BookRepositoryImpl.java
+++ src/repository/BookRepositoryImpl.java
@@ -4,80 +4,104 @@ import model.Book;
 import utils.MyArrayList;
 import utils.MyList;
 
-public class BookRepositoryImpl implements BookRepository{
+/**
+ * Реализация репозитория книг
+ */
+public class BookRepositoryImpl implements BookRepository {
     private final MyList<Book> books;
 
-    // объект, отвечающий за генерацию уникальных id
-//    private final AtomicInteger currentId = new AtomicInteger(1);
-//
     public BookRepositoryImpl() {
         this.books = new MyArrayList<>();
-//        addBooks();
     }
-
-//    private void addBooks() {
-//        books.addAll(
-//                new Book(currentId.getAndIncrement(), "Капитанская дочь", "Пушкин А. С."),
-//                new Book(currentId.getAndIncrement(), "Божественная комедия", "Данте"),
-//                new Book(currentId.getAndIncrement(), "Гамлет", "Шекспир"),
-//                new Book(currentId.getAndIncrement(), "Робинзон Крузо", "Дефо Д."),
-//                new Book(currentId.getAndIncrement(), "Фауст", "Гёте И.В."),
-//                new Book(currentId.getAndIncrement(), "Гордость и предубеждение", "Остин Д.")
-//        );
-//    }
-
-//    @Override
-//    public void addBook(String id, String title, String author) {
-//        Book book = new Book(currentId.getAndIncrement(), title, author);
-//        books.add(book); // сохранение в "хранилище"
-//    }
-
 
     @Override
     public void addBook(String title, String author) {
-
+        // Создаем новую книгу и добавляем в список
+        Book book = new Book(title, author);
+        books.add(book);
     }
 
     @Override
     public MyList<Book> getAllBooks() {
+        // Возвращаем список всех книг
         return books;
     }
 
     @Override
     public MyList<Book> getBooksByAuthor(String author) {
+        // Поиск книг по автору
         MyList<Book> result = new MyArrayList<>();
+        String lowerAuthor = author.toLowerCase();
         for (Book book : books) {
-            if (book.getAuthor().equals(author)) result.add(book);
+            if (book.getAuthor().toLowerCase().contains(lowerAuthor)) {
+                result.add(book);
+            }
         }
         return result;
     }
 
     @Override
     public MyList<Book> getBooksByTitle(String title) {
+        // Поиск книг по названию
         MyList<Book> result = new MyArrayList<>();
+        String lowerTitle = title.toLowerCase();
         for (Book book : books) {
-            if (book.getTitle().equals(title)) result.add(book);
+            if (book.getTitle().toLowerCase().contains(lowerTitle)) {
+                result.add(book);
+            }
         }
         return result;
     }
 
-//    @Override
-//    public MyList<Book> getAvailableBook() {
-//        return null;
-//    }
-
-    // Метод, возвращающий список всех доступных книг
+    @Override
     public MyList<Book> getAllAvailableBooks() {
+        // Получение списка доступных книг
         MyList<Book> result = new MyArrayList<>();
-
         for (Book book : books) {
-            if (book.isAvailable()) result.add(book);
+            if (book.isAvailable()) {
+                result.add(book);
+            }
         }
         return result;
     }
 
     @Override
     public void deleteBook(Book book) {
+        // Удаление книги из списка
         books.remove(book);
+    }
+
+    @Override
+    public void sortByTitle() {
+        // Сортировка книг по названию с использованием пузырьковой сортировки
+        int n = books.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                Book book1 = books.get(j);
+                Book book2 = books.get(j + 1);
+                if (book1.getTitle().compareToIgnoreCase(book2.getTitle()) > 0) {
+                    // Меняем местами книги
+                    books.set(j, book2);
+                    books.set(j + 1, book1);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void sortByAuthor() {
+        // Сортировка книг по автору с использованием пузырьковой сортировки
+        int n = books.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                Book book1 = books.get(j);
+                Book book2 = books.get(j + 1);
+                if (book1.getAuthor().compareToIgnoreCase(book2.getAuthor()) > 0) {
+                    // Меняем местами книги
+                    books.set(j, book2);
+                    books.set(j + 1, book1);
+                }
+            }
+        }
     }
 }
